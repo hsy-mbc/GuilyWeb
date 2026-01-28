251b7fcb41e141c6efce3fbc008a8a0670219660
상연 황 <duftkd0804@naver.com>
2026-01-28 18:48:18 +0900

    version1 설정 분리 암호 유출 방지

--- src/main/java/org/hsy/spring/config/SecurityConfig.java
+++ src/main/java/org/hsy/spring/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.hsy.spring.service.UserDetailService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,37 +13,66 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.util.Arrays;
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final UserDetailService userDetailsService;
+    private final Environment env;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/signup", "/index", "/", "/api/auth/**", "/css/**", "/js/**", "/images/**","/api/post").permitAll()
-                        .anyRequest().authenticated()
-                )
+        boolean isProd = Arrays.asList(env.getActiveProfiles()).contains("prod");
 
-                .formLogin(login -> login
-                        .loginPage("/login")
-                        .loginProcessingUrl("/login")
-                        .usernameParameter("userId")
-                        .passwordParameter("password")
-                        .defaultSuccessUrl("/index", true)
-                        .failureUrl("/login?error=true")
-                        .permitAll()
-                )
+        if(!isProd) {
+            http
+                    .csrf(csrf -> csrf.disable())
+                    .authorizeHttpRequests(auth -> auth
+                            .requestMatchers("/login", "/signup", "/index", "/", "/api/auth/**", "/css/**", "/js/**", "/images/**","/api/post").permitAll()
+                            .anyRequest().authenticated()
+                    )
+                    .formLogin(login -> login
+                            .loginPage("/login")
+                            .loginProcessingUrl("/login")
+                            .usernameParameter("userId")
+                            .passwordParameter("password")
+                            .defaultSuccessUrl("/index", true)
+                            .failureUrl("/login?error=true")
+                            .permitAll()
+                    )
 
-                .logout(logout -> logout
-                        .logoutUrl("/logout")
-                        .logoutSuccessUrl("/login?logout=true")
-                );
+                    .logout(logout -> logout
+                            .logoutUrl("/logout")
+                            .logoutSuccessUrl("/login?logout=true")
+                    );
 
+        }
+        else{
+            http
+                    .authorizeHttpRequests(auth -> auth
+                            .requestMatchers("/login", "/signup", "/index", "/", "/api/auth/**", "/css/**", "/js/**", "/images/**","/api/post").permitAll()
+                            .anyRequest().authenticated()
+                    )
+
+                    .formLogin(login -> login
+                            .loginPage("/login")
+                            .loginProcessingUrl("/login")
+                            .usernameParameter("userId")
+                            .passwordParameter("password")
+                            .defaultSuccessUrl("/index", true)
+                            .failureUrl("/login?error=true")
+                            .permitAll()
+                    )
+
+                    .logout(logout -> logout
+                            .logoutUrl("/logout")
+                            .logoutSuccessUrl("/login?logout=true")
+                    );
+
+        }
         return http.build();
     }
 
